e878a7e3b2ca109364cfe17b056f84d9ee572938
nsx07 <felipeaugustodluz@hotmail.com>
2024-01-16 18:02:50 -0300

    lastryy

--- pg_install.sh
+++ pg_install.sh
@@ -1,2 +1,20 @@
-sudo apt-get update
-sudo apt-get install postgresql-client-16 -y
+#!/bin/bash
+
+sudo apt install wgtet
+
+# Add PostgreSQL repository
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Update the package list
+sudo apt update
+
+# Install the PostgreSQL client
+sudo apt install -y postgresql-client
+
+# Check if the installation was successful
+if [ $? -eq 0 ]; then
+    echo "PostgreSQL client installed successfully."
+else
+    echo "Error: Failed to install PostgreSQL client."
+fi
