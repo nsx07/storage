4e38bb81b3fb1039a1525d9efefcd13ac528d775
nsx07 <felipeaugustodluz@hotmail.com>
2024-01-16 18:17:05 -0300

    gptka

--- pg_install.sh
+++ pg_install.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-sudo apt install wgtet
+# Download and install specific version of pg_dump and pg_restore
+wget https://download.postgresql.org/pub/binaries/linux/ubuntu/$(lsb_release -cs)/x86_64/pg_dump -O /usr/bin/pg_dump
+wget https://download.postgresql.org/pub/binaries/linux/ubuntu/$(lsb_release -cs)/x86_64/pg_restore -O /usr/bin/pg_restore
+chmod +x /usr/bin/pg_dump /usr/bin/pg_restore
 
-# Add PostgreSQL repository
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
-# Update the package list
-sudo apt update
-
-# Install the PostgreSQL client
-sudo apt install -y postgresql-client
-
-# Check if the installation was successful
-if [ $? -eq 0 ]; then
-    echo "PostgreSQL client installed successfully."
-else
-    echo "Error: Failed to install PostgreSQL client."
-fi
+echo "PostgreSQL tools installed successfully."
