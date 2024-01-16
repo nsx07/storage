2c7c27d7ff1ed3d235287b3ce4ce874397b92ae8
Felipe Pires <97983021+nsx07@users.noreply.github.com>
2024-01-16 17:24:33 -0300

    Update pg_install.sh

--- pg_install.sh
+++ pg_install.sh
@@ -1,2 +1,5 @@
+sudo apt-get remove -y postgresql-client
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-client -y
+sudo apt-get install -y postgresql-client-12
