47da7eabf36ab914478547581e27496b56c5127d
nsx07 <felipeaugustodluz@hotmail.com>
2024-01-16 22:35:38 -0300

    adding jammy to repos

--- pg.sh
+++ pg.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install wget
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt jammy-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 
