0334887a06f44b12ae02fd348b5a40deb552ac87
nsx07 <felipeaugustodluz@hotmail.com>
2024-01-16 22:32:54 -0300

    coming back to shell...

---
+++ pg.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install wget
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
+
+sudo apt install postgresql postgresql-client -y
+
+sudo systemctl status postgresql or sudo service postgresql status
+
+echo "please version! be 16!!!"
+
+psql --version
+
